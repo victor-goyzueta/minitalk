c8f4196f18ff26f0bc057609e7e11724f464d88f
victor-goyzueta <vgoyzuet@c3r7s3.42madrid.com>
2025-01-14 18:27:42 +0100

    update

---
+++ inc/client.h
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   client.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vgoyzuet <vgoyzuet@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/14 17:25:06 by vgoyzuet          #+#    #+#             */
+/*   Updated: 2025/01/14 18:27:07 by vgoyzuet         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CLIENT_H
+# define CLIENT_H
+
+# include <unistd.h>
+# include <signal.h>
+# include <stdlib.h>
+# include <sys/types.h>
+# include <sys/time.h>
+
+# define CHAR_0 SIGUSR1
+# define CHAR_1 SIGUSR2
+
+typedef struct s_data
+{
+	pid_t	client_pid;
+	pid_t	server_pid;
+	char	*str;
+}	t_msg;
+
+typedef struct s_globall
+{
+	int						pid;
+	volatile sig_atomic_t	is_ready;
+}	t_global;
+
+//extern t_global	g_server;
+
+//
+
+#endif
---
+++ inc/server.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   server.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vgoyzuet <vgoyzuet@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/14 17:26:35 by vgoyzuet          #+#    #+#             */
+/*   Updated: 2025/01/14 17:28:54 by vgoyzuet         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SERVER_H
+# define SERVER_H
+
+# include <unistd.h>
+# include <signal.h>
+# include <stdlib.h>
+# include <sys/types.h>
+# include <sys/time.h>
+
+# define SIGNAL_RECEIVED SIGUSR2
+
+//
+
+#endif
